40b05bfb6135ee1921ea27fca8284788a6fed937
Andrey Listochkin <andrey.listochkin@gmail.com>
2016-02-22 12:44:07 +0300

    link to a room

--- tests/acceptance/room-list-test.js
+++ tests/acceptance/room-list-test.js
@@ -10,6 +10,16 @@ test('loading a list of rooms', function(assert) {
     assert.equal(currentURL(), '/');
     assert.equal(find('ul').length, 1);
     assert.equal(find('ul li').length, 3);
-    assert.equal(find('ul li:first').text(), 'Welcome');
+    assert.equal(find('ul li:first').text().trim(), 'Welcome');
+  });
+});
+
+test('clicking on a room in the list should open it', function(assert) {
+  visit('/');
+
+  click('ul li:first a');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/r/11/Welcome');
   });
 });
